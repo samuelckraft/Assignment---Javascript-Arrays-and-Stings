4c923c3a2fec8d98bb9d8695801fcf8533c5fb28
samuelckraft <samuelckraft@gmail.com>
2024-06-29 10:20:20 -0400

    update

---
+++ Question 2/script.js
@@ -0,0 +1,25 @@
+// Task 1
+
+let message = "Hello, World!";
+
+console.log(message.length);
+
+// Task 2
+
+let text = "Hello, World!";
+
+console.log(text.toUpperCase());
+
+console.log(text.toLowerCase());
+
+// Task 3
+
+let sentence = "The quick brown fox jumps over the lazy dog";
+
+console.log(sentence.substring(4,20));
+
+// Task 4
+
+let sentence1 = "The quick brown fox";
+
+console.log(sentence1.split(' '))
